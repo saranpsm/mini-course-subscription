651c612039dbacc89ca6c01adb45e19fc5792e04
Saran K <131590642+saranpsm@users.noreply.github.com>
2026-01-04 00:44:18 +0530

    Delete backend directory

--- backend/models/Course.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require("mongoose");
-module.exports = mongoose.model("Course", new mongoose.Schema({
-  title: String,
-  description: String,
-  price: Number,
-  image: String
-}));
